422d03d845e48b26de883397e183224efe6b08fa
minamorl <minamorl@users.noreply.github.com>
2021-04-04 11:24:52 +0900

    Set up apollo-server

---
+++ api/index.ts
@@ -0,0 +1,38 @@
+import { ApolloServer, gql } from "apollo-server"
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling"
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crishton"
+  }
+];
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+};
+
+const main = () => {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+}
+
+main();
