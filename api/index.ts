8a739624334a0027ad9845873551fd2a886e771a
minamorl <minamorl@users.noreply.github.com>
2021-04-16 19:54:40 +0900

    Provide `me` as context

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer, gql, IResolvers } from 'apollo-server'
 import { PrismaClient } from '@prisma/client'
+import { ExpressContext } from 'apollo-server-express/dist/index'
 
 const typeDefs = gql`
   type User {
@@ -15,6 +16,7 @@ const typeDefs = gql`
     content: String
   }
   type Query {
+    me: User
     findUser(name: String!): User
     findUsers(names: [String]!): [User]
     removeUser(name: String!): Boolean
@@ -22,10 +24,14 @@ const typeDefs = gql`
   }
 `
 
-type ContextType = ReturnType<typeof context>
+type Unpromisify<T> = T extends Promise<infer U> ? U : T
+type ContextType = Unpromisify<ReturnType<typeof context>>
 
 const resolvers: IResolvers<void, ContextType> = {
   Query: {
+    me: (_1, _2, context) => {
+      return context.me
+    },
     findUser: (_1, args: { name: string }, context) => {
       return context.prisma.user.findFirst({
         where: {
@@ -55,9 +61,19 @@ const resolvers: IResolvers<void, ContextType> = {
   },
 }
 
-const context = () => ({
-  prisma: new PrismaClient(),
-})
+const context = async ({req}: ExpressContext) => {
+  const prisma = new PrismaClient()
+  const token = req.headers.authorization
+  const me = await prisma.user.findFirst({
+    where: {
+      token
+    }
+  })
+  return {
+    me,
+    prisma,
+  }
+}
 
 const main: () => void = () => {
   const server = new ApolloServer({ typeDefs, resolvers, context })
