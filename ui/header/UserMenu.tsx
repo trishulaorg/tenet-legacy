0ae73e59371dad970137fb5e498f50ef6b1b8edb
minamorl <minamorl@users.noreply.github.com>
2021-04-24 17:03:14 +0900

    Create menu for signed-in user

--- ui/header/UserMenu.tsx
+++ ui/header/UserMenu.tsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react'
 import { observer } from 'mobx-react'
-import { makeStyles, Menu, MenuItem } from '@material-ui/core'
+import { Grid, makeStyles, Menu, MenuItem } from '@material-ui/core'
 import { HeaderStateContext } from './Header'
+import { UserIcon } from './UserIcon'
 
 const useStyles = makeStyles({
   paper: {
     borderRadius: 0,
+    width: '200px',
   },
 })
 
-export type UserMenuProps = { menuVisibility?: boolean }
-
-export const UserMenu: React.FC<UserMenuProps> = observer((props) => {
+export const UserMenu: React.FC = observer(() => {
   const state = useContext(HeaderStateContext)
   const styles = useStyles()
   return (
@@ -19,11 +19,29 @@ export const UserMenu: React.FC<UserMenuProps> = observer((props) => {
       anchorEl={state.menuAnchorElement}
       classes={styles}
       id="ui-menu"
-      open={props.menuVisibility ?? state.menuVisibility}
+      open={state.menuVisibility}
       onClose={() => state.closeMenu()}
     >
-      <MenuItem onClick={() => state.closeMenu()}>Sign up</MenuItem>
-      <MenuItem onClick={() => state.closeMenu()}>Log in</MenuItem>
+      {!state.userState ? (
+        <>
+          <MenuItem onClick={() => state.closeMenu()}>Sign up</MenuItem>
+          <MenuItem onClick={() => state.closeMenu()}>Sign in</MenuItem>
+        </>
+      ) : (
+        <>
+          <MenuItem onClick={() => state.closeMenu()}>
+            <Grid container spacing={1}>
+              <Grid item xs={2}>
+                <UserIcon />
+              </Grid>
+              <Grid item xs="auto">
+                {state.userState.name}
+              </Grid>
+            </Grid>
+          </MenuItem>
+          <MenuItem onClick={() => state.closeMenu()}>Sign out</MenuItem>
+        </>
+      )}
     </Menu>
   )
 })
