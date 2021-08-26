f26afed5a107b8ce4cba51747f9b76cd25078a88
minamorl <minamorl@users.noreply.github.com>
2021-08-26 15:29:43 +0900

    Integrate Storybook with next/router

--- ui/header/UserMenu.tsx
+++ ui/header/UserMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { observer } from 'mobx-react'
+import Link from 'next/link'
 import { Grid, makeStyles, Menu, MenuItem } from '@material-ui/core'
 import { HeaderStateContext } from './Header'
 import { UserIcon } from './UserIcon'
@@ -23,12 +24,16 @@ export const UserMenu: React.FC = observer(() => {
       onClose={() => state.closeMenu()}
     >
       {!state.userState ? (
-        <>
-          <MenuItem onClick={() => state.closeMenu()}>Sign up</MenuItem>
-          <MenuItem onClick={() => state.closeMenu()}>Sign in</MenuItem>
-        </>
+        <div>
+          <MenuItem onClick={() => state.closeMenu()}>
+            <Link href="/signup">Sign up</Link>
+          </MenuItem>
+          <MenuItem onClick={() => state.closeMenu()}>
+            <Link href="/signin">Sign in</Link>
+          </MenuItem>
+        </div>
       ) : (
-        <>
+        <div>
           <MenuItem onClick={() => state.closeMenu()}>
             <Grid container spacing={1}>
               <Grid item xs={2}>
@@ -40,7 +45,7 @@ export const UserMenu: React.FC = observer(() => {
             </Grid>
           </MenuItem>
           <MenuItem onClick={() => state.closeMenu()}>Sign out</MenuItem>
-        </>
+        </div>
       )}
     </Menu>
   )
