507e92844bcec229efc1c10bfb50e61cd78ce3a6
minamorl <minamorl@users.noreply.github.com>
2021-10-04 12:02:14 +0900

    Implement Post/Thread/Reply

--- stories/Thread.stories.tsx
+++ stories/Thread.stories.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 
-import { Thread, ThreadProps } from '../ui/thread/Thread'
+import { Post, PostProps } from '../ui/thread/Thread'
+import { PostState } from '../states/PostState'
 
 export default {
   title: 'Thread/Thread',
-  component: Thread,
+  component: Post,
 } as Meta
 
-const Template: Story<ThreadProps> = (args) => <Thread {...args} />
+const Template: Story<PostProps> = (args) => <Post {...args} />
 
 export const DefaultThread = Template.bind({})
+const post1 = new PostState('Post 1', 'Default Content', { id: '0', name: 'test1' })
+const post2 = new PostState('Thread 1', 'Thread Content', { id: '2', name: 'test2' })
+post2.addResponse(new PostState('Reply 1', 'Reply content', { id: '3', name: 'test3' }))
+post1.addResponse(post2)
+post1.addResponse(new PostState('Thread 2', 'Thread Content', { id: '2', name: 'test2' }))
+post1.addResponse(new PostState('Thread 3', 'Thread Content', { id: '2', name: 'test2' }))
+
 DefaultThread.args = {
-  title: 'Test Thread',
-  content: 'Empty Content',
+  post: post1,
 }
--- ui/thread/Thread.tsx
+++ ui/thread/Thread.tsx
@@ -1,22 +1,66 @@
+import { observer } from 'mobx-react'
 import React from 'react'
+import { PostState } from '../../states/PostState'
 
 import { UserIcon } from '../common/UserIcon'
 
 export interface ThreadProps {
-  title: string
-  content: string
-  author: string
+  posts: PostState[]
 }
 
-export const Thread: React.FC<ThreadProps> = (props) => {
+export interface PostProps {
+  post: PostState
+}
+
+export const Reply: React.FC<ThreadProps> = observer((props) => {
+  return (
+    <ul className="pl-8 border-gray-200	border-l-4">
+      {props.posts.map((v, i) => {
+        return (
+          <li key={i} className="py-4">
+            <div className="text-xl">{v.title}</div>
+            <div>
+              <UserIcon size="small" />
+              {v.author.name}
+            </div>
+            <div>{v.content}</div>
+          </li>
+        )
+      })}
+    </ul>
+  )
+})
+
+export const Thread: React.FC<ThreadProps> = observer((props) => {
+  return (
+    <ul className="pl-4 bg-gray-50">
+      {props.posts.map((v, i) => {
+        return (
+          <li key={i} className="py-4">
+            <div className="text-xl">{v.title}</div>
+            <div>
+              <UserIcon size="small" />
+              {v.author.name}
+            </div>
+            <div>{v.content}</div>
+            {v.hasRepsponse ? <Reply posts={v.responses} /> : undefined}
+          </li>
+        )
+      })}
+    </ul>
+  )
+})
+
+export const Post: React.FC<PostProps> = observer((props) => {
   return (
     <div className="bg-white filter drop-shadow p-4">
-      <div className="text-2xl">{props.title}</div>
+      <div className="text-2xl">{props.post.title}</div>
       <div>
         <UserIcon size="small" />
-        {props.author}
+        {props.post.author.name}
       </div>
-      <div>{props.content}</div>
+      <div>{props.post.content}</div>
+      {props.post.hasRepsponse ? <Thread posts={props.post.responses} /> : undefined}
     </div>
   )
-}
+})
