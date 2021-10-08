34ac145fc1aebebe09f9505d5238fd765d1855e0
minamorl <minamorl@users.noreply.github.com>
2021-10-09 05:09:15 +0900

    Integrate with GraphQL

--- ui/header/Header.tsx
+++ ui/header/Header.tsx
@@ -3,15 +3,18 @@ import { observer } from 'mobx-react'
 import { Title } from './Title'
 import { HeaderState } from '../../states/HeaderState'
 import { UserMenu } from './UserMenu'
+import { UserState } from '../../states/UserState'
 
-export type HeaderProps = Record<string, unknown>
+export interface HeaderProps {
+  user: UserState
+}
 
 export const HeaderStateContext = createContext(new HeaderState())
 
-export const HeaderImpl = observer((props: React.PropsWithChildren<HeaderProps>) => {
-  props // wip
+export const HeaderImpl = observer(() => {
   const state = useContext(HeaderStateContext)
-  state // wip
+  state.userState?.request()
+
   return (
     <nav className="flex item-center justify bg-gray-600 px-4 py-2">
       <Title value="Coton" />
@@ -22,8 +25,8 @@ export const HeaderImpl = observer((props: React.PropsWithChildren<HeaderProps>)
 
 export const Header: React.FC<HeaderProps> = (props) => {
   return (
-    <HeaderStateContext.Provider value={new HeaderState()}>
-      <HeaderImpl {...props} />
+    <HeaderStateContext.Provider value={new HeaderState(props.user)}>
+      <HeaderImpl />
     </HeaderStateContext.Provider>
   )
 }
