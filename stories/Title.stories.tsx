c5d31d4c264faf922725f9397091ed8cf3f997cf
minamorl <minamorl@users.noreply.github.com>
2021-10-10 02:34:42 +0900

    Add SvgLogo for Coton

--- stories/Title.stories.tsx
+++ stories/Title.stories.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 
-import { Title, TitleProps } from '../ui/header/Title'
+import { Title } from '../ui/header/Title'
 
 export default {
   title: 'Header/Title',
   component: Title,
 } as Meta
 
-const Template: Story<TitleProps> = (args) => <Title {...args} />
+const Template: Story = () => <Title />
 
 export const DefaultTitle = Template.bind({})
-DefaultTitle.args = {
-  value: 'Title',
-}
+DefaultTitle.args = {}
