ab6f891353e952cd53d935c2a0f3efc4c34256c2
minamorl <minamorl@users.noreply.github.com>
2021-10-11 18:59:47 +0900

    Implement toggle menu

--- ui/header/UserMenu.tsx
+++ ui/header/UserMenu.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { observer } from 'mobx-react'
-import { HeaderStateContext } from './Header'
+import { HeaderStateContext } from '../../states/HeaderState'
 import { MenuIcon } from '@heroicons/react/solid'
 
 export const UserMenu: React.FC = observer(() => {
@@ -10,29 +10,56 @@ export const UserMenu: React.FC = observer(() => {
       <div className="flex-initial mx-3 my-auto flex items-center justify-center">
         <button className="my-auto" onClick={() => state.toggleMenu()}>
           <MenuIcon className="h-5 w-5 text-white" />
-          <UserMenuItems loggedIn={state.isLoggedIn} visibility={state.menuVisibility} />
+          <UserMenuItems>
+            <Switch visibility={state.isLoggedIn}>
+              <UserMenuItem onClick={() => state.togglePersonaList()}>
+                {state.userState?.currentPersona.name}
+                <Switch visibility={state.personaListVisibility}>
+                  <ul className="bg-white width-100">
+                    {state.userState?.personas.map((p, idx) => (
+                      <li key={idx} className="border-solid border-b-2">
+                        <button
+                          onClick={() =>
+                            state.userState && (state.userState.currentPersonaIndex = idx)
+                          }
+                        >
+                          {p.name}
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </Switch>
+              </UserMenuItem>
+              <UserMenuItem onClick={() => undefined}>Sign out</UserMenuItem>
+            </Switch>
+            <Switch visibility={!state.isLoggedIn}>
+              <UserMenuItem onClick={() => undefined}>Sign up</UserMenuItem>
+            </Switch>
+          </UserMenuItems>
         </button>
       </div>
     </>
   )
 })
 
-export interface UserMenuItemsProps {
-  loggedIn: boolean
-  visibility: boolean
-}
-
-export const UserMenuItems: React.FC<UserMenuItemsProps> = observer((props) => {
-  const loggedOutMenuItems = (
+export const UserMenuItems: React.FC = observer((props) => {
+  return (
     <div className="relative inline-block float-right">
-      <ul
-        className={`${
-          props.visibility ? '' : 'hidden'
-        } absolute right-0 z-10 bg-white w-48 rounded-sm border-2`}
-      >
-        <li className="p-4">Sign In</li>
+      <ul className={`absolute right-0 z-10 bg-white w-48 rounded-sm border-2`}>
+        {props.children}
       </ul>
     </div>
   )
-  return loggedOutMenuItems
+})
+
+export const Switch: React.FC<{ visibility: boolean }> = observer((props) => {
+  return <>{props.visibility ? props.children : undefined}</>
+})
+
+export const UserMenuItem: React.FC<{ onClick: () => void }> = observer((props) => {
+  return (
+    <li className="p-4">
+      <button onClick={() => props.onClick()}>{props.children}</button>
+    </li>
+  )
 })
