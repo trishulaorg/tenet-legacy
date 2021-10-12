5f4bbadf87846360fd38ca660162c71a63f971b1
minamorl <minamorl@users.noreply.github.com>
2021-10-12 14:41:12 +0900

    Refactor/Improve UI

---
+++ ui/common/Switch.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { observer } from 'mobx-react'
+
+export const Switch: React.FC<{ visibility: boolean }> = observer((props) => {
+  return <>{props.visibility ? props.children : undefined}</>
+})
--- ui/header/UserMenu.tsx
+++ ui/header/UserMenu.tsx
@@ -1,41 +1,46 @@
-import React, { useContext } from 'react'
+import React, { MouseEventHandler, useContext } from 'react'
 import { observer } from 'mobx-react'
 import { HeaderStateContext } from '../../states/HeaderState'
 import { MenuIcon } from '@heroicons/react/solid'
+import { Switch } from '../common/Switch'
 
 export const UserMenu: React.FC = observer(() => {
   const state = useContext(HeaderStateContext)
   return (
     <>
       <div className="flex-initial mx-3 my-auto flex items-center justify-center">
-        <button className="my-auto" onClick={() => state.toggleMenu()}>
-          <MenuIcon className="h-5 w-5 text-white" />
-          <UserMenuItems>
-            <Switch visibility={state.isLoggedIn}>
-              <UserMenuItem onClick={() => state.togglePersonaList()}>
-                {state.userState?.currentPersona.name}
-                <Switch visibility={state.personaListVisibility}>
-                  <ul className="bg-white width-100">
-                    {state.userState?.personas.map((p, idx) => (
-                      <li key={idx} className="border-solid border-b-2">
-                        <button
-                          onClick={() =>
-                            state.userState && (state.userState.currentPersonaIndex = idx)
-                          }
-                        >
-                          {p.name}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
-                </Switch>
-              </UserMenuItem>
-              <UserMenuItem onClick={() => undefined}>Sign out</UserMenuItem>
-            </Switch>
-            <Switch visibility={!state.isLoggedIn}>
-              <UserMenuItem onClick={() => undefined}>Sign up</UserMenuItem>
-            </Switch>
-          </UserMenuItems>
+        <button className="my-auto">
+          <button onClick={() => state.toggleMenu()}>
+            <MenuIcon className="h-5 w-5 text-white" />{' '}
+          </button>
+          <Switch visibility={state.menuVisibility}>
+            <UserMenuItems>
+              <Switch visibility={state.isLoggedIn}>
+                <UserMenuItem onClick={() => state.togglePersonaList()}>
+                  {state.userState?.currentPersona.name}
+                  <Switch visibility={state.personaListVisibility}>
+                    <ul className="bg-white width-100">
+                      {state.userState?.personas.map((p, idx) => (
+                        <li key={idx} className="border-solid border-b-2">
+                          <button
+                            onClick={() => {
+                              state.userState && (state.userState.currentPersonaIndex = idx)
+                            }}
+                          >
+                            {p.name}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </Switch>
+                </UserMenuItem>
+                <UserMenuItem onClick={() => state.closeMenu()}>Sign out</UserMenuItem>
+              </Switch>
+              <Switch visibility={!state.isLoggedIn}>
+                <UserMenuItem onClick={() => state.closeMenu()}>Sign up</UserMenuItem>
+              </Switch>
+            </UserMenuItems>
+          </Switch>
         </button>
       </div>
     </>
@@ -52,14 +57,10 @@ export const UserMenuItems: React.FC = observer((props) => {
   )
 })
 
-export const Switch: React.FC<{ visibility: boolean }> = observer((props) => {
-  return <>{props.visibility ? props.children : undefined}</>
-})
-
-export const UserMenuItem: React.FC<{ onClick: () => void }> = observer((props) => {
+export const UserMenuItem: React.FC<{ onClick?: MouseEventHandler }> = observer((props) => {
   return (
     <li className="p-4">
-      <button onClick={() => props.onClick()}>{props.children}</button>
+      <button onClick={(e) => props.onClick?.(e)}>{props.children}</button>
     </li>
   )
 })
