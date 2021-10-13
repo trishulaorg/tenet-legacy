11a4d50b042dd1e7100ff38e05fa027e0d9295a1
tsukaryu <drgon8otsuka@gmail.com>
2021-10-13 17:58:48 +0900

    fix: コンポーネントの細分化

--- ui/home/ActivityCard.tsx
+++ ui/home/ActivityCard.tsx
@@ -1,46 +1,24 @@
 import React from 'react'
-import {
-  TrendingUpIcon,
-  TrendingDownIcon,
-  AnnotationIcon,
-  ShareIcon,
-  BookmarkIcon,
-} from '@heroicons/react/solid'
-import { UserIcon } from '../common/UserIcon'
+import { PersonaState } from '../../states/UserState'
+import { Author } from './Author'
+import { ActivityCardTitle } from './ActivityCardTitle'
+import { ActivityCardContent } from './ActivityCardContent'
+import { ActivityCardFooter } from './ActivityCardFooter'
 
-export const ActivityCard: React.FC = (props: any) => {
+export const ActivityCard: React.FC<{ title: string; content: string; author: PersonaState }> = ({
+  title,
+  content,
+  author,
+}) => {
   return (
     <div className="max-w-2xl rounded-lg p-4 bg-white">
-      <h2 className="text-3xl pb-2">{props.title}</h2>
-      <div className="flex items-center pb-2">
-        <UserIcon size={'small'} />
-        <p className="pl-1">{props.author.name}</p>
-      </div>
-      <div className="px-8 pb-8">
-        <p>{props.content}</p>
-      </div>
-      <div className="flex">
-        <div className="flex items-center opacity-50">
-          <TrendingUpIcon className="h-5 w-5" />
-          <p className="pl-1">1500</p>
-        </div>
-        <div className="flex items-center pl-1 opacity-50">
-          <TrendingDownIcon className="h-5 w-5" />
-          <p className="pl-1">1500</p>
-        </div>
-        <div className="flex items-center pl-4 opacity-50">
-          <AnnotationIcon className="h-5 w-5" />
-          <p className="pl-1">1k Comment</p>
-        </div>
-        <div className="flex items-center pl-4 opacity-50">
-          <ShareIcon className="h-5 w-5" />
-          <p className="pl-1">Share</p>
-        </div>
-        <div className="flex items-center pl-4 opacity-50">
-          <BookmarkIcon className="h-5 w-5" />
-          <p className="pl-1">Save</p>
-        </div>
-      </div>
+      <ActivityCardTitle title={title} />
+      <Author name={author.name} iconUrl="http://example.com/" /> {/* TODO: replace iconUrl */}
+      <ActivityCardContent content={content} />
+      <ActivityCardFooter />
+      {/* <ActivityCardMeta>
+        <CreatedAt created={someInstanceOfDateTime} />
+      </ActivityCardMeta> */}
     </div>
   )
 }
