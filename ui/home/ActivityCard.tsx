c64b30319b1ce30159bb6120ca24bf5fcc7db1e8
tsukaryu <drgon8otsuka@gmail.com>
2021-10-13 18:31:06 +0900

    fix: ActiveCardMetaの追加

--- ui/home/ActivityCard.tsx
+++ ui/home/ActivityCard.tsx
@@ -3,22 +3,26 @@ import { PersonaState } from '../../states/UserState'
 import { Author } from './Author'
 import { ActivityCardTitle } from './ActivityCardTitle'
 import { ActivityCardContent } from './ActivityCardContent'
-import { ActivityCardFooter } from './ActivityCardFooter'
+import { ActivityCardIcons } from './ActivityCardIcons'
+import { ActivityCardMeta } from './ActivityCardMeta'
+import { CreatedAt } from './CreatedAt'
 
 export const ActivityCard: React.FC<{ title: string; content: string; author: PersonaState }> = ({
   title,
   content,
   author,
 }) => {
+  const time = new Date().toISOString()
   return (
     <div className="max-w-2xl rounded-lg p-4 bg-white">
       <ActivityCardTitle title={title} />
       <Author name={author.name} iconUrl="http://example.com/" /> {/* TODO: replace iconUrl */}
       <ActivityCardContent content={content} />
-      <ActivityCardFooter />
-      {/* <ActivityCardMeta>
-        <CreatedAt created={someInstanceOfDateTime} />
-      </ActivityCardMeta> */}
+      <ActivityCardMeta>
+        <ActivityCardIcons />
+        <div className="pb-2"></div>
+        <CreatedAt created={time} />
+      </ActivityCardMeta>
     </div>
   )
 }
---
+++ ui/home/ActivityCardMeta.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ActivityCardMeta: React.FC = (props) => {
+  return <div>{props.children}</div>
+}
