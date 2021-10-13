a626e725101684326e3666e941116d3190090a76
tsukaryu <drgon8otsuka@gmail.com>
2021-10-13 18:35:15 +0900

    add: observer

--- ui/home/ActivityCard.tsx
+++ ui/home/ActivityCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { observer } from 'mobx-react'
 import { PersonaState } from '../../states/UserState'
 import { Author } from './Author'
 import { ActivityCardTitle } from './ActivityCardTitle'
@@ -7,22 +8,19 @@ import { ActivityCardIcons } from './ActivityCardIcons'
 import { ActivityCardMeta } from './ActivityCardMeta'
 import { CreatedAt } from './CreatedAt'
 
-export const ActivityCard: React.FC<{ title: string; content: string; author: PersonaState }> = ({
-  title,
-  content,
-  author,
-}) => {
-  const time = new Date().toISOString()
-  return (
-    <div className="max-w-2xl rounded-lg p-4 bg-white">
-      <ActivityCardTitle title={title} />
-      <Author name={author.name} iconUrl="http://example.com/" /> {/* TODO: replace iconUrl */}
-      <ActivityCardContent content={content} />
-      <ActivityCardMeta>
-        <ActivityCardIcons />
-        <div className="pb-2"></div>
-        <CreatedAt created={time} />
-      </ActivityCardMeta>
-    </div>
-  )
-}
+export const ActivityCard: React.FC<{ title: string; content: string; author: PersonaState }> =
+  observer(({ title, content, author }) => {
+    const time = new Date().toISOString()
+    return (
+      <div className="max-w-2xl rounded-lg p-4 bg-white">
+        <ActivityCardTitle title={title} />
+        <Author name={author.name} iconUrl="http://example.com/" /> {/* TODO: replace iconUrl */}
+        <ActivityCardContent content={content} />
+        <ActivityCardMeta>
+          <ActivityCardIcons />
+          <div className="pb-2"></div>
+          <CreatedAt created={time} />
+        </ActivityCardMeta>
+      </div>
+    )
+  })
