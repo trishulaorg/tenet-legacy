c650fe56b0c6b6f8fcfe46d03e674857ccc2b469
tsukaryu <drgon8otsuka@gmail.com>
2021-10-18 16:13:02 +0900

    fix: rename files

--- ui/common/CardContent.tsx
+++ ui/common/CardContent.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const ActivityCardContent: React.FC<{ content: string }> = ({ content }) => {
+export const CardContent: React.FC<{ content: string }> = ({ content }) => {
   return (
     <div className="px-8 pb-8">
       <p>{content}</p>
