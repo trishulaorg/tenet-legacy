6bb4b1b3199601f45ada4125168da0594370211f
tsukaryu <drgon8otsuka@gmail.com>
2021-10-26 11:27:51 +0900

    fix: type

--- ui/home/HomeTab.tsx
+++ ui/home/HomeTab.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 
-export const HomeTab: React.FC<{ onClick: () => void; selected: boolean }> = observer((props) => {
+export const HomeTab: React.FC<{
+  onClick: React.MouseEventHandler
+  selected: boolean
+}> = observer((props) => {
   const style = props.selected
     ? 'text-xl font-bold mr-5 pb-1 border-solid border-b-2 border-gray-600 border-opacity-80'
     : 'text-xl font-bold mr-5 pb-1 border-solid border-b-2 border-gray-600 border-opacity-80 opacity-50'
