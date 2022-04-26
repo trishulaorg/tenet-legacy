6aa7b3dab49a354e2281f98fcaf882f67ce1aa5b
minamorl <minamorl@users.noreply.github.com>
2022-04-26 13:23:36 +0900

    Add search page design

---
+++ pages/s/[word].tsx
@@ -0,0 +1,67 @@
+import { useEffect } from 'react'
+import { getGqlToken } from '../../libs/cookies'
+import { HeaderState, HeaderStateContext } from '../../states/HeaderState'
+import { defaultUser, UserState } from '../../states/UserState'
+import { Header } from '../../ui/header/Header'
+import { Layout } from '../../ui/layouts/Layout'
+
+const SearchResultList: React.FC = (props) => {
+  return <ul>{props.children}</ul>
+}
+
+const IndexPage: React.FC = () => {
+  const token = getGqlToken()
+  let user = defaultUser()
+  if (token) user = new UserState(token, [], 0)
+  useEffect(() => {
+    const f = async (): Promise<void> => {
+      if (user) {
+        await user.request()
+      }
+    }
+    f()
+  }, [token, user])
+
+  const dummy = {
+    result: new Array(100).fill({
+      title: 'board title',
+    }),
+  }
+  const main: React.FC = () => (
+    <>
+      <h1 className="text-xl">Search Result</h1>
+      <SearchResultList>
+        {dummy.result.map((c, idx) => (
+          <li
+            key={idx}
+            className="flex my-2 p-2 rounded bg-white/75 hover:bg-white cursor-pointer border"
+          >
+            <div className="w-8 text-slate-400">#{idx + 1}</div>
+            <div className="flex-1 text-slate-700">
+              <div className="text-2xl	text-slate-900">{c.title}</div>
+              <div className="">Kind: Board</div>
+            </div>
+          </li>
+        ))}
+      </SearchResultList>
+    </>
+  )
+  return (
+    <div className="bg-gray-100">
+      <HeaderStateContext.Provider value={new HeaderState(user)}>
+        <Header></Header>
+      </HeaderStateContext.Provider>
+      <Layout Main={main} Side={() => <div className="max-w-xs">test</div>} />
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next {
+          height: 100%;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default IndexPage
