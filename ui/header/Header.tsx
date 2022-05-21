e6fa94ccdbe4308da438a9b021a71c7b90c60c31
minamorl <minamorl@users.noreply.github.com>
2022-05-22 04:29:12 +0900

    Add searchbox

--- ui/header/Header.tsx
+++ ui/header/Header.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { observer } from 'mobx-react'
 import { Title } from './Title'
 import { UserMenu } from './UserMenu'
+import { SearchBox } from './SearchBox'
 
 export const Header = observer(() => {
   return (
     <nav className="flex item-center justify bg-white px-4 py-2 shadow-md">
       <Title />
+      <SearchBox />
       <UserMenu />
     </nav>
   )
