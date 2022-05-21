4b73624940baca66adfee21aff892d72c654d354
minamorl <minamorl@users.noreply.github.com>
2022-05-22 03:14:13 +0900

    Fix

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,27 @@
 import { ApolloServer } from 'apollo-server-micro'
 import { typeDefs, resolvers, context } from '../../server'
+import Cors from 'micro-cors'
 
-const server = new ApolloServer({ typeDefs, resolvers, context })
+const cors = Cors()
 
-export default server.createHandler({ path: '/api/graphql' })
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context })
+
+const startServer = apolloServer.start()
+
+export default cors(async function handler(req: any, res: any) {
+  if (req.method === 'OPTIONS') {
+    res.end()
+    return false
+  }
+  await startServer
+
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+})
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
