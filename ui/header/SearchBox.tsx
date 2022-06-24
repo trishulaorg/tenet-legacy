f0b62a2ce35b14379da8568399ab33649d0379be
minamorl <minamorl@users.noreply.github.com>
2022-06-24 17:18:43 +0900

    Make compatible with mobile

--- ui/header/SearchBox.tsx
+++ ui/header/SearchBox.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import React, { KeyboardEventHandler, useState } from 'react'
 
+import { SearchIcon } from '@heroicons/react/solid'
 export const SearchBox = () => {
   const router = useRouter()
   const [text, setText] = useState('')
@@ -9,14 +10,17 @@ export const SearchBox = () => {
     if (e.code === 'Enter') move()
   }
   return (
-    <div>
+    <div className="border rounded flex">
       <input
-        className="border rounded"
         type="text"
         value={text}
         onChange={(e) => setText(e.currentTarget.value)}
         onKeyDown={onKeyDown}
+        className="flex-1 outline-0"
       />
+      <button onClick={move}>
+        <SearchIcon className="h-5 w-5 text-gray-400 hover:text-gray-700 "></SearchIcon>
+      </button>
     </div>
   )
 }
