bc2dda37c72a54298218b89929e9737c58c40cd8
minamorl <minamorl@users.noreply.github.com>
2022-08-01 05:05:19 +0900

    Fix auth0 config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   env: {
-    AUTH0_BASE_URL: process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
+    AUTH0_BASE_URL: process.env.VERCEL_URL || 'http://localhost:3000'
   }
 };
