8a4a666ecb4f22d0af965471cc2375af972b545a
minamorl <minamorl@users.noreply.github.com>
2022-08-08 10:11:48 +0900

    Fix user default icon

--- ui/common/UserIcon.tsx
+++ ui/common/UserIcon.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { UserIcon as HUserIcon } from '@heroicons/react/solid'
 
 export interface UserIconProps {
   size: 'small' | 'medium'
@@ -9,12 +10,6 @@ export const UserIcon: React.FC<UserIconProps> = ({ size, src }) => {
   return !src ? (
     <div className={size === 'small' ? 'w-5 h-5 border rounded-sm inline-block' : 'w-40 h-40'} />
   ) : (
-    <img
-      src={src ? src : 'https://picsum.photos/20'}
-      alt="avater"
-      width={size ? '20px' : '160px'}
-      height={size ? '20px' : '160px'}
-      referrerPolicy={'no-referrer'}
-    />
+    <HUserIcon width={size ? '20px' : '160px'} height={size ? '20px' : '160px'} />
   )
 }
