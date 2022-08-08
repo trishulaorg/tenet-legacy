8d6c2d087dc52cb317e1de6e9ee07fd19b826166
minamorl <minamorl@users.noreply.github.com>
2022-08-08 10:23:52 +0900

    Fix

--- ui/common/UserIcon.tsx
+++ ui/common/UserIcon.tsx
@@ -8,8 +8,8 @@ export interface UserIconProps {
 
 export const UserIcon: React.FC<UserIconProps> = ({ size, src }) => {
   return !src ? (
-    <div className={size === 'small' ? 'w-5 h-5 border rounded-sm inline-block' : 'w-40 h-40'} />
-  ) : (
     <HUserIcon width={size ? '20px' : '160px'} height={size ? '20px' : '160px'} />
+  ) : (
+    <img src={src} alt="avater" width={size ? '20px' : '160px'} height={size ? '20px' : '160px'} />
   )
 }
