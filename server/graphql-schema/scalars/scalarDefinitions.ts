645fa4ec05af4c441573d9151adb42d8555a44ee
Lagyu <sasaki.y@ruri.waseda.jp>
2022-08-16 10:55:10 +0900

    fix: fixed upload was not working

--- server/graphql-schema/scalars/scalarDefinitions.ts
+++ server/graphql-schema/scalars/scalarDefinitions.ts
@@ -2,12 +2,13 @@ import { asNexusMethod, scalarType } from 'nexus'
 import { GraphQLDate } from 'graphql-iso-date'
 import { GraphQLError, GraphQLScalarType } from 'graphql'
 import { BadRequestError } from '../../errors/BadRequest/BadRequestError'
-import type { FileUpload } from 'graphql-upload'
+import type { FileUpload, Upload } from 'graphql-upload'
 import { DateTimeResolver } from 'graphql-scalars'
 
-const isFileUpload = (value: unknown): value is FileUpload => {
-  return !!value && typeof (value as FileUpload).createReadStream === 'function'
+const isUpload = (value: unknown): value is Upload => {
+  return !!value && typeof (value as Upload).file === 'object'
 }
+
 export const UploadScalar = scalarType({
   name: 'Upload',
   description: 'Upload type compatible with graphql-upload',
@@ -15,10 +16,15 @@ export const UploadScalar = scalarType({
     throw new GraphQLError('Upload serialization unsupported.')
   },
   parseValue: async (value: unknown): Promise<FileUpload> => {
-    const upload = await value
-    if (!isFileUpload(upload)) {
+    if (!isUpload(value)) {
       throw new BadRequestError('file data is broken.')
     }
+
+    const upload = await value.file
+    if (typeof upload === 'undefined') {
+      throw new BadRequestError('file data is undefined.')
+    }
+
     const stream = upload.createReadStream()
     const fileType = await (await import('file-type')).fileTypeFromStream(stream)
 
@@ -34,9 +40,9 @@ export const UploadScalar = scalarType({
 
 // @ts-expect-error hoge
 const GQLUpload = asNexusMethod(UploadScalar, 'upload')
-type Upload = File
+type UploadType = File
 const GQLDate = asNexusMethod(GraphQLDate, 'date')
 const GQLDate2 = asNexusMethod(new GraphQLScalarType(DateTimeResolver), 'dateTime')
 
 export { GQLUpload, GQLDate, GQLDate2 }
-export type { Upload }
+export type { UploadType as Upload }
