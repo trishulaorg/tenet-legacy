27ad6c143c3ae761ed14eef39100c6a26417d4f8
Lagyu <sasaki.y@ruri.waseda.jp>
2022-08-18 14:14:45 +0900

    ci: added api test case

--- pages/api/__tests__/graphql/sample.test.ts
+++ pages/api/__tests__/graphql/sample.test.ts
@@ -1,22 +1,14 @@
-// import { getSdk } from '../../../../server/frontend-graphql-definition'
-// import { GraphQLClient } from 'graphql-request'
-//
-// const baseApiUrl = 'http://localhost:3000/api/graphql'
-//
-// const apiTestClient = getSdk(new GraphQLClient(baseApiUrl))
-//
-// describe('sample test', () => {
-//   test('activities return value', async () => {
-//     const { activities } = await apiTestClient.getActivities()
-//
-//     expect(true).toEqual(true)
-//   })
-// })
+import { getSdk } from '../../../../server/frontend-graphql-definition'
+import { GraphQLClient } from 'graphql-request'
 
-describe('Sample', () => {
-  test('Always Success', async () => {
-    expect(true).toEqual(true)
+const baseApiUrl = 'http://localhost:3000/api/graphql'
+
+const apiTestClient = getSdk(new GraphQLClient(baseApiUrl))
+
+describe('sample test', () => {
+  test('activities return value', async () => {
+    const { activities } = await apiTestClient.getActivities()
+
+    expect(activities).toEqual([])
   })
 })
-
-export {}
