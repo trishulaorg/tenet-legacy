b90e2760416e451d0e1ad69a001947469e0c4595
Lagyu <sasaki.y@ruri.waseda.jp>
2022-09-05 13:54:53 +0900

    fix: fix icon collapsed on small screens

--- ui/board/Board.tsx
+++ ui/board/Board.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { ChatIcon, MinusCircleIcon, PlusCircleIcon } from '@heroicons/react/solid'
 import { MultiLineText } from '../common/MultiLineText'
 
-interface BoardProps {
+type BoardProps = {
   showPostCreate?: boolean
   followButtonType?: 'follow' | 'unfollow'
   onFollowButtonClick?: () => Promise<void>
@@ -19,39 +19,41 @@ export const Board: React.FC<BoardProps> = observer(
 
     return (
       <div>
-        <div className="flex">
-          <h1 className="my-4 text-slate-600 text-2xl flex-1">
+        <div className="flex flex-col">
+          <h1 className="flex-row my-4 text-slate-600 text-2xl">
             <Link href={`/b/${state.id}`}>
               <span className="cursor-pointer">#{state.title}</span>
             </Link>
           </h1>
-          {onFollowButtonClick ? (
-            followButtonType === 'follow' ? (
-              <button
-                onClick={onFollowButtonClick}
-                className="my-4 py-2 px-8 block text-white bg-teal-400 hover:bg-teal-600	rounded-xl border border-slate-300 flex"
-              >
-                <PlusCircleIcon width={24} />
-                <span className="px-2">Follow</span>
-              </button>
-            ) : (
-              <button
-                onClick={onFollowButtonClick}
-                className="my-4 py-2 px-8 block text-white bg-gray-400 hover:bg-gray-700	rounded-xl border border-slate-300 flex"
-              >
-                <MinusCircleIcon width={24} />
-                <span className="px-2">Unfollow</span>
-              </button>
-            )
-          ) : null}
-          {showPostCreate ? (
-            <Link href={{ pathname: `/o/cp`, query: { boardId: state.id } }}>
-              <button className="ml-4 my-4 py-2 px-8 block text-white bg-teal-400 hover:bg-teal-600	rounded-xl border border-slate-300 flex">
-                <ChatIcon width={24} />
-                <span className="px-2">New Post</span>
-              </button>
-            </Link>
-          ) : null}
+          <div className={'flex ml-auto'}>
+            {onFollowButtonClick ? (
+              followButtonType === 'follow' ? (
+                <button
+                  onClick={onFollowButtonClick}
+                  className="flex mb-4 py-2 px-2 md:px-4 lg:px-6 block text-white bg-teal-400 hover:bg-teal-600 rounded-xl border border-slate-300"
+                >
+                  <PlusCircleIcon width={24} />
+                  <span className="px-2">Follow</span>
+                </button>
+              ) : (
+                <button
+                  onClick={onFollowButtonClick}
+                  className="flex mb-4 py-2 px-2 md:px-4 lg:px-6 block text-white bg-gray-400 hover:bg-gray-700 rounded-xl border border-slate-300"
+                >
+                  <MinusCircleIcon width={24} />
+                  <span className="px-2">Unfollow</span>
+                </button>
+              )
+            ) : null}
+            {showPostCreate ? (
+              <Link href={{ pathname: `/o/cp`, query: { boardId: state.id } }}>
+                <button className="flex ml-2 lg:ml-4 mb-4 py-2 px-2 md:px-4 lg:px-6 block text-white bg-teal-400 hover:bg-teal-600 rounded-xl border border-slate-300">
+                  <ChatIcon width={24} />
+                  <span className="px-2">New Post</span>
+                </button>
+              </Link>
+            ) : null}
+          </div>
         </div>
         <div>
           <MultiLineText text={state.description} />
