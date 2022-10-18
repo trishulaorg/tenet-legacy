530c7b34f64d99400101c1074d252aa5ecd4e011
minamorl <minamorl@users.noreply.github.com>
2022-10-18 17:41:00 +0900

    Fix type

--- pages/api/__tests__/graphql/libs/client.ts
+++ pages/api/__tests__/graphql/libs/client.ts
@@ -4,8 +4,7 @@ import { PrismaClient } from '@prisma/client'
 
 const BASE_API_URL = 'http://localhost:3000/api/graphql'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const generateAPITestClient = (headers: HeadersInit) =>
+const generateAPITestClient = (headers: HeadersInit): ReturnType<typeof getSdk> =>
   getSdk(new GraphQLClient(BASE_API_URL, { headers }))
 
 const prismaClient = new PrismaClient()
