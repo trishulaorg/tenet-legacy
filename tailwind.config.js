6327936d864a15e6a2daf84247170ef38b3fb6bd
Mitchell Sharma <sharmamitch@gmail.com>
2022-11-21 23:03:53 -0600

    Defined color palette for light/dark themes Added variables for text and background elements to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,32 @@ module.exports = {
       colors: {
         'primary': {
           'DEFAULT': '#33AEBC',
-          'dark': '#116999',
+          'dark': '#116999'
         },
         'secondary': '#00EB9C',
         'teritary': '#EB0C54',
+        'pagebg': { /* background for empty space on the page */
+          'DEFAULT': '#F3F4F6',
+          'darkmode' : 'black'
+        },
+        'contentbg': { /* background for posts, following board, navbar, dropdowns, etc. (background for text) */
+          'DEFAULT' : 'white',
+          'darkmode' : '#2E2E2E'
+        },
+        'fontcl' : {
+          'light' : { /* @Usernames, light text elements */
+            'DEFAULT' : '#9CA3AF',
+            'darkmode' : 'white'
+          }, 
+          'med' : { /* Most text, including board titles, post titles and post content */
+            'DEFAULT' : '#374151',
+            'darkmode' : 'white'
+          },
+          'heavy' : { /* Bold text: Following boards title, TOS */
+            'DEFAULT' : 'black',
+            'darkmode' : 'white'
+          } 
+        }
       }
     },
   },
