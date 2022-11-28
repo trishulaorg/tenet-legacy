31b2b40a237f63a01b2ce96840c3b9c9dd7287df
minamorl <minamorl@users.noreply.github.com>
2022-11-28 19:47:04 +0900

    select-none

--- ui/post/Post.tsx
+++ ui/post/Post.tsx
@@ -81,7 +81,7 @@ export const Post: React.FC<PostProps> = observer(({ post, showThreads }) => {
     <motion.div
       initial={{ y: 10, opacity: 0, borderRadius: 100 }}
       animate={{ y: 0, opacity: 0.9, borderRadius: 20 }}
-      className="drop-shadow-lg rounded-lg p-4 bg-contentbg dark:bg-contentbg-dark transition-colors duration-350"
+      className="select-none drop-shadow-lg rounded-lg p-4 bg-contentbg dark:bg-contentbg-dark transition-colors duration-350"
       layout
     >
       <CardTitle title={post.title} />
