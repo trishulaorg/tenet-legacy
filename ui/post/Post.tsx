8c910e2b5adaa7fc9fa260694a47d56cacc4109f
minamorl <minamorl@users.noreply.github.com>
2022-11-28 19:43:27 +0900

    change opacity

--- ui/post/Post.tsx
+++ ui/post/Post.tsx
@@ -80,7 +80,7 @@ export const Post: React.FC<PostProps> = observer(({ post, showThreads }) => {
   const content = (
     <motion.div
       initial={{ y: 10, opacity: 0, borderRadius: 100 }}
-      animate={{ y: 0, opacity: 1, borderRadius: 20 }}
+      animate={{ y: 0, opacity: 0.9, borderRadius: 20 }}
       className="drop-shadow-lg rounded-lg p-4 bg-contentbg dark:bg-contentbg-dark transition-colors duration-350"
       layout
     >
