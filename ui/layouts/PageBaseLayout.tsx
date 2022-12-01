b6e2023cedb814827eea9052f6413ae6c3d0dc34
gutchom <yskz.tg@gmail.com>
2022-12-01 14:01:26 +0900

    fix background gap

--- ui/layouts/PageBaseLayout.tsx
+++ ui/layouts/PageBaseLayout.tsx
@@ -1,7 +1,20 @@
 import type { FC } from 'react'
+import React from 'react'
 
 const PageBaseLayout: FC = ({ children }) => (
-  <div className="transition-colors duration-350 h-max min-h-screen">{children}</div>
+  <div className="transition-colors duration-350 h-max min-h-screen">
+    <div
+      style={{
+        backgroundImage: "url('/wallpaper.jpg')",
+        filter: 'blur(5px)',
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+      }}
+    />
+
+    {children}
+  </div>
 )
 
 export { PageBaseLayout }
