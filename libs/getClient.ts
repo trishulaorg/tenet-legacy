2522d6917e6b5bab8bb94f37412faeb1e951f2d2
minamorl <minamorl@users.noreply.github.com>
2022-12-06 09:35:41 +0900

    useTenet

--- libs/getClient.ts
+++ libs/getClient.ts
@@ -3,4 +3,13 @@ import useSWR from 'swr'
 import operations from '../schema/operation.graphql'
 import { swrKey } from './swrKey'
 
-export { client, swrKey, operations }
+
+const endpoint = 'https://coton.vercel.app/api/graphql'
+const key = (name: string, variables: Record<string, unknown>) => (swrKey[name]!)(variables)
+const useTenet: (args: {token?: string, operationName:string, variables: Record<string, unknown>}) => ReturnType<typeof useSWR> = ({token, operationName, variables}) => {
+  return useSWR(key(operationName, variables), () => client(endpoint, operations[operationName], variables, {
+     authorization: 'Bearer '  + token ?? "INVALID_TOKEN"
+  }))
+}
+
+export { client, swrKey, operations, useTenet }
