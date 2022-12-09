bf0024ef7784bf5131622bb3a2d97c6ae8930db9
gutchom <yskz.tg@gmail.com>
2022-12-09 10:32:41 +0900

    refactor pages/search/[word].tsx according to PageContentLayout.tsx

--- pages/search/[word].tsx
+++ pages/search/[word].tsx
@@ -41,36 +41,38 @@ const IndexPage: NextPage<{ initialData: any }> = ({ initialData }) => {
     fallbackData: initialData,
   })
 
-  const main: React.FC = () => (
-    <PageBaseLayout>
-      <h1 className="text-xl">Search Result</h1>
-      <SearchResultList>
-        {data &&
-          data.search.map((c, idx) => (
-            <li
-              key={c.id}
-              className="flex my-2 p-2 rounded bg-contentbg/75 hover:bg-contentbg dark:bg-contentbg-dark/75 dark:hover:bg-contentbg-dark transition-colors duration-350 cursor-pointer border dark:border-med"
-            >
-              <div className="w-8 text-low dark:text-low-dark">#{idx + 1}</div>
-              <div className="flex-1 text-med dark:text-med-dark">
-                <div className="text-2xl	text-high dark:text-high-dark">
-                  <Link href={`/board/${c.id}`} legacyBehavior>
-                    {c.title}
-                  </Link>
-                </div>
-                <div className="">Kind: {c.kind}</div>
-              </div>
-            </li>
-          ))}
-      </SearchResultList>
-    </PageBaseLayout>
-  )
   return (
     <div className="bg-pagebg dark:bg-pagebg-dark transition-colors duration-350">
       <HeaderStateContext.Provider value={new HeaderState(user)}>
         <Header />
       </HeaderStateContext.Provider>
-      <PageContentLayout Main={main} Side={() => <div className="max-w-xs">test</div>} />
+      <PageContentLayout
+        main={
+          <PageBaseLayout>
+            <h1 className="text-xl">Search Result</h1>
+            <SearchResultList>
+              {data &&
+                data.search.map((c, idx) => (
+                  <li
+                    key={c.id}
+                    className="flex my-2 p-2 rounded bg-contentbg/75 hover:bg-contentbg dark:bg-contentbg-dark/75 dark:hover:bg-contentbg-dark transition-colors duration-350 cursor-pointer border dark:border-med"
+                  >
+                    <div className="w-8 text-low dark:text-low-dark">#{idx + 1}</div>
+                    <div className="flex-1 text-med dark:text-med-dark">
+                      <div className="text-2xl	text-high dark:text-high-dark">
+                        <Link href={`/board/${c.id}`} legacyBehavior>
+                          {c.title}
+                        </Link>
+                      </div>
+                      <div className="">Kind: {c.kind}</div>
+                    </div>
+                  </li>
+                ))}
+            </SearchResultList>
+          </PageBaseLayout>
+        }
+        side={<div className="max-w-xs">test</div>}
+      />
     </div>
   )
 }
