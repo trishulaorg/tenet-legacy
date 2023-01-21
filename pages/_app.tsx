01cf25a477dddfb40f898279706abed3c9fdbcf3
gutchom <yskz.tg@gmail.com>
2023-01-22 00:49:41 +0900

    fix: _app.tsx code about login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import '../styles/global.css'
 import { PageBaseLayout } from '../ui/layouts/PageBaseLayout'
 import { Header } from '../ui/header/Header'
@@ -14,14 +13,10 @@ import jwt from 'jsonwebtoken'
 
 export default function App({ Component, pageProps }: AppProps) {
   const user = getUser()
-  const router = useRouter()
-  const [, setPersonaId] = useState<number | undefined>(undefined)
 
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     ;(async () => {
-      const r = init()
-      const { auth } = r
+      const { auth } = init()
       if (!isValidAuthInstance(auth) || !auth.currentUser) return
       if (getCookies().has('gqltoken') && getCookies().get('gqltoken') !== '') {
         user.token = getCookies().get('gqltoken') ?? ''
