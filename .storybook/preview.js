e28cd35e5b9439243c6e63f2b981110666e57d52
Keito Omura <1119kate@gmail.com>
2023-02-11 18:44:25 +0900

    Add msw-storybook-addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,14 @@ import '../styles/global.css'
 import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css'
 import { themes } from '@storybook/theming'
 import { RouterContext } from "next/dist/shared/lib/router-context";
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+// Initialize MSW
+initialize({
+  onUnhandledRequest: 'bypass',
+});
+// Provide the MSW addon decorator globally
+export const decorators = [mswDecorator];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,6 +22,7 @@ export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider,
   },
+  decorators: [ mswDecorator ],
   darkMode: {
     // Override the default dark theme
     dark: { ...themes.dark, appBg: 'black' },
