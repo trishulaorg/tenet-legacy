e30c7d9ce4ef127357c21f8f918c2066c4852850
minamorl <minamorl@users.noreply.github.com>
2023-02-12 11:51:10 +0900

    InputWithLabel/PerosnaIconForm/SuccessMessage

---
+++ stories/form/PersonaIconForm.stories.tsx
@@ -0,0 +1,7 @@
+import { PersonaIconForm } from '../../ui/settings/PersonaIconForm'
+
+export default {
+  title: 'Settings/PersonaIconForm',
+  component: PersonaIconForm,
+}
+export const PersonaIconFormStory = () => <PersonaIconForm />
---
+++ stories/form/SuccessMessage.stories.tsx
@@ -0,0 +1,7 @@
+import { SuccessMessage } from '../../ui/form/SuccessMessage'
+
+export default {
+  title: 'Form/SuccessMessage',
+  component: SuccessMessage,
+}
+export const SuccessMessageStory = () => <SuccessMessage successMessage="Rolem ipusum" />
