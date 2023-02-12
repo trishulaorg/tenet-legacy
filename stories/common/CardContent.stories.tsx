b32cf86cccbc5025d2f100cd568074f51fd81244
minamorl <minamorl@users.noreply.github.com>
2023-02-12 09:57:41 +0900

    CardContent stories

---
+++ stories/common/CardContent.stories.tsx
@@ -0,0 +1,12 @@
+import type { ComponentStory } from '@storybook/react'
+import { ComponentMeta } from '@storybook/react'
+import { CardContent } from '../../ui/common/CardContent'
+
+export default {
+  title: 'common/CardContewnt',
+  component: CardContent,
+} satisfies ComponentMeta<typeof CardContent>
+
+export const main: ComponentStory<typeof CardContent> = () => (
+  <CardContent content={'Lorem'} imageUrls={[' https://via.placeholder.com/150']} />
+)
