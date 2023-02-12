be4a3cabe2b9ea8c290209495ceeeaedc358ec26
minamorl <minamorl@users.noreply.github.com>
2023-02-12 10:50:54 +0900

    Add Card meta

---
+++ stories/common/CardMeta.stories.tsx
@@ -0,0 +1,9 @@
+import { CardMeta } from '../../ui/common/CardMeta'
+
+export default {
+  title: 'Common/CardMeta',
+  component: CardMeta,
+}
+// Write storybook story about <CardMeta>
+// starts from export
+export const CardMetaStory = () => <CardMeta isPost={true}>Rolem ipusum</CardMeta>
