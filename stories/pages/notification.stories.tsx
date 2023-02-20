83904c9f7d1abef2646cb406f3a1f2972fb9f7e3
Mitchell Sharma <sharmamitch@gmail.com>
2023-02-20 16:25:03 -0600

    Added notification page story

---
+++ stories/pages/notification.stories.tsx
@@ -0,0 +1,8 @@
+import NotificationPage from '../../pages/notification'
+
+export default {
+  title: 'Notification/NotificationPage',
+  component: NotificationPage,
+}
+
+export const Default = () => <NotificationPage />
