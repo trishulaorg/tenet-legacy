ff069173f08cdd1d502ae0dffdba99b287093be2
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-16 15:49:25 -0500

    Styled settings page, adjusted margins, integrated SettingsTab component

--- ui/settings/Settings.tsx
+++ ui/settings/Settings.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { SettingsTab } from './SettingsTab'
 
 //TODO: Change buttons to tabs: on click, change to tab for that settings category
 //TODO: Implement form for settings, including custom text inputs, checkboxes, and submit button
@@ -6,21 +7,20 @@ import React from 'react'
 
 export const Settings = () => {
   return (
-    <div className="m-4 p-2 bg-contentbg dark:bg-contentbg-dark flex flex-row items-center">
-      <div className="my-4 text-med dark:text-med-dark text-2xl">
-        <div>
-          <h1>Account Settings</h1>
-          <button>Contact information</button>
-          <button>Personas</button>
-          <button>Notifications</button>
+    <div className="m-4 p-2 bg-contentbg dark:bg-contentbg-dark flex flex-row items-center rounded-lg shadow-lg">
+      <div className="my-4 pl-2 pr-4 text-med dark:text-med-dark text-2xl border-r-2 border-low dark:border-low-dark">
+        <div className="py-2">
+          <h1 className="font-semibold">Account Settings</h1>
+          <SettingsTab label="Contact Information" />
+          <SettingsTab label="Personas" />
+          <SettingsTab label="Notifications" />
         </div>
-        <div>
-          <h1>Interface Settings</h1>
-          <button>Theme</button>
-          <button>Background</button>
+        <div className="py-2">
+          <h1 className="font-semibold">Interface Settings</h1>
+          <SettingsTab label="Theme" />
+          <SettingsTab label="Background" />
         </div>
       </div>
-      <div className="my-4 text-med dark:text-med-dark text-2xl" />
     </div>
   )
 }
