21e26dea3ba5b3a3a2660ffb4adb0845b9bfbdfe
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-17 17:57:02 -0500

    Settings page progress: Implemeted dynamic settings form based on current tab

--- ui/settings/Settings.tsx
+++ ui/settings/Settings.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SettingsTab } from './SettingsTab'
 import { SettingsForm } from './SettingsForm'
 
 //TODO: Change buttons to tabs: on click, change to tab for that settings category
 //TODO: Implement form for settings, including custom text inputs, checkboxes, and submit button
 //TODO: Make responsive for mobile (change settings categories to submenus below settings)
-//TODO: Fix height (currently using h-screen, but scrollbar always appears due to margin and nav)
+//TODO: Fix height (currently hardcoding at 768px, want it dynamic with margins at sides for "floating card" style)
 
 export const Settings = () => {
+  const [currentTab, setCurrentTab] = useState('contact')
+
+  const handleTabClick = (tab: string) => () => {
+    setCurrentTab(tab)
+  }
+
   return (
-    <div className="m-4 h-screen bg-contentbg dark:bg-contentbg-dark flex flex-row items-center rounded-lg shadow-lg">
+    <div className="m-4 h-full bg-contentbg dark:bg-contentbg-dark flex flex-row justify-start items-center rounded-lg shadow-lg">
       <div className="h-full py-6 px-4 text-med dark:text-med-dark text-2xl border-r-[3px] border-low dark:border-low-dark">
         <div className="py-2">
           <h1 className="font-semibold">Account Settings</h1>
-          <SettingsTab label="Contact Information" />
-          <SettingsTab label="Personas" />
-          <SettingsTab label="Notifications" />
+          <SettingsTab label="Contact Information" onClick={() => handleTabClick('contact')} />
+          <SettingsTab label="Personas" onClick={() => handleTabClick('personas')} />
+          <SettingsTab label="Notifications" onClick={() => handleTabClick('notifications')} />
         </div>
         <div className="py-2">
           <h1 className="font-semibold">Interface Settings</h1>
-          <SettingsTab label="Theme" />
-          <SettingsTab label="Background" />
+          <SettingsTab label="Theme" onClick={() => handleTabClick('theme')} />
+          <SettingsTab label="Background" onClick={() => handleTabClick('background')} />
         </div>
       </div>
-      <div className="my-4 pl-2 pr-4 text-med dark:text-med-dark text-2xl h-full py-6 px-4">
-        <SettingsForm
-          formLabel="Contact Information"
-          inputs={[
-            { inputLabel: 'Email', inputType: 'text', inputPlaceholder: 'my_email@email.com' },
-            { inputLabel: 'Password', inputType: 'password', inputPlaceholder: '********' },
-          ]}
-        />
+      <div className="my-4 px-4 text-med dark:text-med-dark text-2xl h-full py-6">
+        <SettingsForm currentTab={currentTab} />
       </div>
     </div>
   )
--- ui/settings/SettingsForm.tsx
+++ ui/settings/SettingsForm.tsx
@@ -1,33 +1,89 @@
-import React from 'react'
-
-export interface FormInput {
-  inputLabel?: string
-  inputType?: 'text' | 'password' | 'checkbox'
-  inputPlaceholder?: string
-}
+import React, { useState } from 'react'
 
 interface SettingsFormProps {
-  formLabel: string
-  inputs: FormInput[]
-  submit?: () => void
+  currentTab: 'contact' | 'personas' | 'notifications' | 'theme' | 'background' | string
 }
 
 export const SettingsForm = (props: SettingsFormProps) => {
-  const { formLabel, inputs, submit } = props
-  return (
-    <form className="">
-      <h1 className="text-3xl font-normal">{formLabel}</h1>
-      {inputs.map((input) => (
-        <div className="flex flex-col">
-          <label className="mt-1 text-lg font-normal">{input.inputLabel}</label>
-          <input
-            type={input.inputType}
-            placeholder={input.inputPlaceholder}
-            className="indent-1 text-lg rounded-md border-2 border-med dark:border-med-dark my-1"
-          />
-        </div>
-      ))}
-      <button onClick={submit}>Submit</button>
-    </form>
-  )
+  const { currentTab } = props
+
+  if (currentTab === 'contact') {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+
+    const handleEmailChange = (email: string) => {
+      console.log('Email changed, new email: ', email)
+    }
+
+    const handlePasswordChange = (password: string) => {
+      console.log('Password changed, new password: ', password)
+    }
+
+    return (
+      <div>
+        <form className="">
+          <h1 className="text-3xl font-normal">Change Email Address</h1>
+          <div className="flex flex-col">
+            <label className="mt-1 text-lg font-normal">New Email Address</label>
+            <input
+              type="text"
+              placeholder="my_email@email.com"
+              className="indent-1 text-lg rounded-md border-2 border-med dark:border-med-dark my-1 placeholder-low dark:placeholder-low-dark text-high dark:text-high-dark bg-pagebg dark:bg-pagebg-dark"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button
+            className="mt-2 py-1 px-8 border-med dark:border-med-dark border-2 rounded-full bg-pagebg dark:bg-pagebg-dark text-med dark:text-med-dark text-base font-semibold"
+            onClick={() => handleEmailChange(email)}
+          >
+            Submit
+          </button>
+        </form>
+
+        <form className="mt-8">
+          <h1 className="text-3xl font-normal">Change Password</h1>
+          <div className="flex flex-col">
+            <label className="mt-1 text-lg font-normal">Current Password</label>
+            <input
+              type="password"
+              placeholder="********"
+              className="indent-1 text-lg rounded-md border-2 border-med dark:border-med-dark my-1 placeholder-low dark:placeholder-low-dark text-high dark:text-high-dark bg-pagebg dark:bg-pagebg-dark"
+            />
+            <label className="mt-1 text-lg font-normal">New Password</label>
+            <input
+              type="password"
+              placeholder="********"
+              className="indent-1 text-lg rounded-md border-2 border-med dark:border-med-dark my-1 placeholder-low dark:placeholder-low-dark text-high dark:text-high-dark bg-pagebg dark:bg-pagebg-dark"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label className="mt-1 text-lg font-normal">Confirm New Password</label>
+            <input
+              type="password"
+              placeholder="********"
+              className="indent-1 text-lg rounded-md border-2 border-med dark:border-med-dark my-1 placeholder-low dark:placeholder-low-dark text-high dark:text-high-dark bg-pagebg dark:bg-pagebg-dark"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <button
+            className="mt-2 py-1 px-8 border-med dark:border-med-dark border-2 rounded-full bg-pagebg dark:bg-pagebg-dark text-med dark:text-med-dark text-base font-semibold"
+            onClick={() => handlePasswordChange(password)}
+            disabled={password !== confirmPassword}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  } else if (currentTab === 'personas') {
+    return <div></div>
+  } else if (currentTab === 'notifications') {
+    return <div></div>
+  } else if (currentTab === 'theme') {
+    return <div></div>
+  } else if (currentTab === 'background') {
+    return <div></div>
+  } else {
+    return <div></div>
+  }
 }
