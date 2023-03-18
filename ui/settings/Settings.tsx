1548af3416232b59f88ae4f1cc869f61c81195de
Mitchell Sharma <sharmamitch@gmail.com>
2023-03-17 19:19:53 -0500

    Added color transition to settings component (for theme switch)

--- ui/settings/Settings.tsx
+++ ui/settings/Settings.tsx
@@ -25,7 +25,7 @@ export const Settings = () => {
   const [currentTab, setCurrentTab] = useState('contact')
 
   return (
-    <div className="m-4 h-full bg-contentbg dark:bg-contentbg-dark flex flex-row justify-start items-center rounded-lg shadow-lg">
+    <div className="m-4 h-full bg-contentbg dark:bg-contentbg-dark flex flex-row justify-start items-center rounded-lg shadow-lg transition-colors">
       <div className="h-full py-6 px-4 text-med dark:text-med-dark text-2xl border-r-[3px] border-low dark:border-low-dark">
         <div className="py-2">
           <h1 className="font-semibold">Account Settings</h1>
